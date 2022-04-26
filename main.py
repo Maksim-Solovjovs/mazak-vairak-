2236ef0850e79173884055aa534dc48611022bf6
Max <77140683+Maksim-Solovjovs@users.noreply.github.com>
2022-04-26 08:15:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+a = int(input('Ievadi skaitlu a: '))
+b = int(input('Īevadi skaitlu b: '))
+if a > b:
+  print(a, "vairāk neka", b)
+elif a < b:
+  print(a, "mazāk neka", b)
+else: print(a, "ir vienāds ar", b)
